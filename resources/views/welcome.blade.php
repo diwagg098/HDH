f88ad1ba3f971de296ca4cb33808ed497b7889e0
diwarunna <diwagg098@gmail.com>
2021-09-29 10:35:34 +0700

    menambahkan rating pada welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,17 +164,8 @@
                 <img src="{{ 'http://localhost/dashboard-hdh/public/images/product/' . $product->foto}}" alt="" width="100">
                 <div class="shop-col1">
                     <h1>{{ $product->nama_product}}</h1>
-                    <div class="rate"> 
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label for="star5" title="text">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4" />
-                    <label for="star4" title="text">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3" />
-                    <label for="star3" title="text">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2" />
-                    <label for="star2" title="text">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1" />
-                    <label for="star1" title="text">1 star</label>
+                    <div class="rate">
+                        <p>Rating : 5 / 5</p>
                     
                 </div>
                 <p>Rp. {{ number_format($product->harga,0,',','.')}}</p>
