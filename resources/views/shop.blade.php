f4e72df1e32504823241bbb477dd7ffe240e3fa5
diwarunna <diwagg098@gmail.com>
2021-10-01 14:14:55 +0700

    second commit

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -17,17 +17,7 @@
                 <div class="shop-col1">
                     <h1>{{ $row->nama_product}}</h1>
                     <div class="rate"> 
-                        {{-- <form action=""> --}}
-                        <input type="radio" id="star5" name="rate" value="5" />
-                        <label for="star5" title="text">5 stars</label>
-                        <input type="radio" id="star4" name="rate" value="4" />
-                        <label for="star4" title="text">4 stars</label>
-                        <input type="radio" id="star3" name="rate" value="3" />
-                        <label for="star3" title="text">3 stars</label>
-                        <input type="radio" id="star2" name="rate" value="2" />
-                        <label for="star2" title="text">2 stars</label>
-                        <input type="radio" id="star1" name="rate" value="1" />
-                        <label for="star1" title="text">1 star</label>
+                        <p>Rate 5/5</p>
                     {{-- </form> --}}
                         
                       </div>
